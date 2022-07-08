eb28eb36a2f5b8584f4bbccffc41fa36e32872c2
kdv1995 <tribeofdanel@gmail.com>
2022-07-08 09:47:00 +0300

    08/07/2022_09:46

--- src/CreateTable.jsx
+++ src/CreateTable.jsx
@@ -1,17 +1,31 @@
 import React from "react";
-const matrix = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
-];
+import { useSelector } from "react-redux";
+
+// const matrix = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9],
+// ];
+
 const CreateTable = () => {
+  const rows = useSelector((state) => state.rows);
+  // for (let i = 0; i < 1; i++) {
+  //   const row = matrix[i];
+  //   for (let j = 0; j < 3; j++) {
+  //     matrix[i][j] = Math.floor(Math.random() * (999 - 100 + 1) + 100);
+  //   }
+  //   matrix.push(row);
+  // }
+  // console.log(matrix);
   return (
-    <div style={{ display: "flex", flexWrap: "wrap", width: 120 }}>
-      <>
-        {matrix.map((submatrix) =>
-          submatrix.map((elem) => <button>{elem}</button>)
-        )}
-      </>
+    <div style={{ display: "flex" }}>
+      {/* {matrix.map((submatrix, index) => (
+        <div key={index}>
+          {submatrix.map((elem, key) => (
+            <button key={key}>{elem}</button>
+          ))}
+        </div>
+      ))} */}
     </div>
   );
 };
