1144b8df7b5b7f8628d882d8364974cd60ac72cc
kdv1995 <tribeofdanel@gmail.com>
2022-07-11 17:36:11 +0300

    11/07/2022_17:36

--- src/components/Matrix/CreateTable/CreateTable.jsx
+++ src/components/Matrix/CreateTable/CreateTable.jsx
@@ -8,8 +8,8 @@ const CreateTable = () => {
   const rows = useSelector((state) => state.rows);
   const columns = useSelector((state) => state.columns);
   const cells = useSelector((state) => state.cells);
-
   const navigate = useNavigate();
+
   let matrix = [];
   for (let i = 0; i < rows; i++) {
     matrix[i] = [];
@@ -21,25 +21,44 @@ const CreateTable = () => {
     <div className={s.CreateTable}>
       <Heading title="Matrix" />
       <div className={s.CreateTableContainer}>
-        {matrix.map((r, index) => (
-          <div style={{ display: "flex" }}>
-            {r.map((clmn, index) => (
-              <div>
-                <div className={s.Value}>
-                  <div>{clmn}</div>
-                </div>
-              </div>
-            ))}
-          </div>
-        ))}
-        <div>
-          <Button
-            onClick={() => {
-              navigate("/matrix");
-            }}
-            title="Set another data"
-          />
+        <div style={{ display: "flex", justifyContent: "space-around" }}>
+          {matrix.map((_, index) => (
+            <span>{index + 1}</span>
+          ))}
         </div>
+        <div>
+          {matrix.map((row, rowIndex) => (
+            <div style={{ display: "flex" }}>
+              <div style={{ display: "flex", alignItems: "center" }}>
+                {rowIndex + 1}
+              </div>
+              {row.map((column) => (
+                <>
+                  <div>
+                    <div className={s.CellValue}>{column}</div>
+                  </div>
+                </>
+              ))}
+              <div
+                className={s.RowSum}
+                style={{ display: "flex", alignItems: "center" }}
+              >
+                {row.reduce((a, b) => a + b)}
+              </div>
+            </div>
+          ))}
+          <div>
+            {matrix.reduce((a, b) => a.map((x, i) => (x + b[i]) / rows.length))}
+          </div>
+        </div>
+      </div>
+      <div>
+        <Button
+          onClick={() => {
+            navigate("/matrix");
+          }}
+          title="Set another data"
+        />
       </div>
     </div>
   );
